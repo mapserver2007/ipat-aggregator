71a9074ae35c81a682f5f1418620ada19ea2b905
mapserver2007 <mapserver2007@gmail.com>
2024-02-05 18:52:16 +0900

    データ更新

--- app/usecase/data_cache_usecase/data_cache.go
+++ app/usecase/data_cache_usecase/data_cache.go
@@ -20,7 +20,7 @@ const (
 	jockeyFileName            = "jockey.json"
 	raceIdFileName            = "race_id.json"
 	predictRaceResultFilePath = "predict_races/race_result_%d.json"
-	startDate                 = "20231203"
+	startDate                 = "20231112"
 	endDate                   = "20240131"
 )
 

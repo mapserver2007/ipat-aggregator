fdbf04737ea77b808804515326b9224a62242b58
mapserver2007 <mapserver2007@gmail.com>
2025-04-17 22:08:48 +0900

    データ更新

--- config/config.go
+++ config/config.go
@@ -5,13 +5,13 @@ const (
 	CacheDir = "cache"
 	// race_idマスタ、各oddsマスタ
 	RaceStartDate = "20230729"
-	RaceEndDate   = "20250322"
+	RaceEndDate   = "20250412"
 	// race_time用設定
 	RaceTimeStartDate = "20230729"
-	RaceTimeEndDate   = "20250330"
+	RaceTimeEndDate   = "20250413"
 	// 分析用設定
 	AnalysisUnHitWinLowerOdds = 2.0 // 未満設定
 	// 予想用設定
-	PredictionSyncRaceDate          = "20250406"
+	PredictionSyncRaceDate          = "20250412"
 	PredictionCheckListWinLowerOdds = 2.9
 )
